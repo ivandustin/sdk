6468e428bf9079ab6bf8ef2f72a9b53f81b53afe
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-07 16:26:23 +0800

    Add image on exg dock

--- etc/docks/exg/handle.sh
+++ etc/docks/exg/handle.sh
@@ -3,4 +3,6 @@ set -euvxo pipefail
 lang=$1
 export FBPROF=$2
 export FBPUB=$3
-sandbox exgx | log | ifa trans $lang | log | fbpost
+exgx | log | ifa trans $lang | log > post.txt
+ifa trans $lang < topic.txt | txt2img img.png
+fbpost --media img.png < post.txt
--- etc/docks/exg/install.sh
+++ etc/docks/exg/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euvxo pipefail
-pip install playwright
+pip install playwright chevron
 playwright install chromium
 playwright install-deps
 git clone https://github.com/ivandustin/sdk.git
