4efc8d28020a370b8d1306a835b6969036b6b1eb
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-07 17:07:09 +0800

    Update exg dock

--- etc/docks/exg/handle.sh
+++ etc/docks/exg/handle.sh
@@ -3,6 +3,6 @@ set -euvxo pipefail
 lang=$1
 export FBPROF=$2
 export FBPUB=$3
-exgx | log | ifa trans $lang | log > post.txt
-ifa trans $lang < topic.txt | txt2img img.png
-fbpost --media img.png < post.txt
+exgx | log | ifa trans $lang | log > content.txt
+sen < content.txt | log | ifa trans $lang | log | txt2img title.png
+fbpost --media title.png < content.txt
