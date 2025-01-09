73cfcc6413d34639e6d8fe673d1c222cb6850075
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-09 15:28:59 +0800

    Add lay to gstory dock

--- etc/docks/gstory/handle.sh
+++ etc/docks/gstory/handle.sh
@@ -3,6 +3,6 @@ set -euvxo pipefail
 lang=$1
 export FBPROF=$2
 export FBPUB=$3
-gstoryx | log | ifa trans $lang | log > content.txt
+gstoryx | log | lay | log | ifa trans $lang | log > content.txt
 sen < content.txt | log | ifa trans $lang | log | txt2img title.png
 fbpost --media title.png < content.txt
