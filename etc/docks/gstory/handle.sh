494ee783b8abebac4290b028fecd81f4e2d05f1b
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-09 14:23:49 +0800

    Add gstory dock

---
+++ etc/docks/gstory/handle.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euvxo pipefail
+lang=$1
+export FBPROF=$2
+export FBPUB=$3
+gstoryx | log | ifa trans $lang | log > content.txt
+sen < content.txt | log | ifa trans $lang | log | txt2img title.png
+fbpost --media title.png < content.txt
---
+++ etc/docks/gstory/pre.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -euvx
+cp $PWSTOR pw.json
