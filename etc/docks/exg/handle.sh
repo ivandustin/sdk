2b33d6b013889f5587127f99685f2be062c13c2d
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-13 12:38:13 +0000

    Remove lay in exg dock

--- etc/docks/exg/handle.sh
+++ etc/docks/exg/handle.sh
@@ -3,6 +3,6 @@ set -euvxo pipefail
 lang=$1
 export FBPROF=$2
 export FBPUB=$3
-exgx | log | lay | log | ifa trans $lang | log > content.txt
+exgx | log | ifa trans $lang | log > content.txt
 sen < content.txt | log | ifa trans $lang | log | txt2img title.png
 fbpost --media title.png < content.txt
