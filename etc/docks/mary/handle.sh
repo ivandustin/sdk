c2e5a3c2dc920c7559306891ff62ab27bf2b11b8
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-01-14 08:51:11 +0000

    Update handle in mary dock

--- etc/docks/mary/handle.sh
+++ etc/docks/mary/handle.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euvxo pipefail
-lang=$1
+export LANGUAGE=$1
 export FBPROF=$2
 export FBPUB=$3
-andrew | log | ifa trans $lang | log > content.txt
-philip content.txt
-sen < content.txt | log | ifa trans $lang | log | txt2img title.png
-fbpost --media title.png < content.txt
+exec mary
