5d81c25d15e2063f04a0b945dafceced90acb8d3
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-05-07 16:39:42 +0000

    Shorten arg names in approx

--- lib/py/approx/fn.py
+++ lib/py/approx/fn.py
@@ -2,9 +2,9 @@ from flax.nnx import Module, Rngs, Linear, sigmoid
 
 
 class Fn(Module):
-    def __init__(self, inputs, outputs, neurons, rngs: Rngs):
-        self.a = Linear(inputs, neurons, rngs=rngs)
-        self.b = Linear(neurons, outputs, rngs=rngs)
+    def __init__(self, din, dout, width, rngs: Rngs):
+        self.a = Linear(din, width, rngs=rngs)
+        self.b = Linear(width, dout, rngs=rngs)
 
     def __call__(self, x):
         return self.b(sigmoid(self.a(x)))
