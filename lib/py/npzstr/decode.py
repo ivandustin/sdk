ec822e9446d7541685ad1b6267e613bed1d5095f
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-08-20 12:17:39 +0800

    Do not sort decode npzstr

--- lib/py/npzstr/decode.py
+++ lib/py/npzstr/decode.py
@@ -5,4 +5,4 @@ from numpy import ndarray, load
 
 def decode(batch: str) -> list[ndarray]:
     with load(BytesIO(b64decode(batch))) as result:
-        return [result[key] for key in sorted(result.files)]
+        return [result[key] for key in result.files]
