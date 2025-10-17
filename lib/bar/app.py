b560eb1a68aa6d34f0c5a8770e3fcb070e4246bf
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-10-17 22:32:01 +0800

    Add bar

---
+++ lib/bar/app.py
@@ -0,0 +1,21 @@
+from sys import argv
+from streamlit import set_page_config, cache_data, text_input, plotly_chart
+from plotly.express import bar
+from numpy import loadtxt
+
+filepath = argv[1]
+set_page_config(layout="wide")
+array = cache_data(loadtxt)(filepath, dtype=str, ndmin=2)
+y, x = list(map(int, array[:, 0])), array[:, 1]
+highlight = text_input("Highlight")
+figure = bar(x=x, y=y)
+if highlight:
+    figure.add_shape(
+        type="line",
+        x0=highlight,
+        y0=0,
+        x1=highlight,
+        y1=max(y),
+        line=dict(color="orange", dash="dot"),
+    )
+plotly_chart(figure)
