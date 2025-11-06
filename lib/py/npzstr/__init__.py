439266db99c4225e42632058a9bb2fcd8b6cb41d
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-11-06 17:58:55 +0000

    Remove npzstr

--- lib/py/npzstr/__init__.py
+++
@@ -1,4 +0,0 @@
-from .encode import encode
-from .decode import decode
-
-__all__ = ["encode", "decode"]
--- lib/py/npzstr/decode.py
+++
@@ -1,8 +0,0 @@
-from base64 import b64decode
-from io import BytesIO
-from numpy import ndarray, load
-
-
-def decode(batch: str) -> list[ndarray]:
-    with load(BytesIO(b64decode(batch))) as result:
-        return [result[key] for key in result.files]
