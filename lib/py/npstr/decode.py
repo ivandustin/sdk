b8895f1cf35b64982b066b8c570239291a3cf4a0
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-11-06 17:41:12 +0000

    Remove npstr

--- lib/py/npstr/decode.py
+++
@@ -1,7 +0,0 @@
-from base64 import b64decode
-from io import BytesIO
-from numpy import ndarray, load
-
-
-def decode(string: str) -> ndarray:
-    return load(BytesIO(b64decode(string)))
--- lib/py/npstr/encode.py
+++
@@ -1,9 +0,0 @@
-from base64 import b64encode
-from io import BytesIO
-from numpy import ndarray, save
-
-
-def encode(array: ndarray) -> str:
-    buffer = BytesIO()
-    save(buffer, array)
-    return b64encode(buffer.getvalue()).decode("utf-8")
