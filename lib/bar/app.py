e61cb9449f302688f8b0382a1032fa97f88dc842
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-11-12 22:59:40 +0800

    Update bar

--- lib/bar/app.py
+++ lib/bar/app.py
@@ -1,12 +1,12 @@
-from sys import argv
+from sys import stdin
 from streamlit import set_page_config, cache_data, text_input, plotly_chart
 from plotly.express import bar
 from numpy import loadtxt
 
-filepath = argv[1]
 set_page_config(layout="wide")
-array = cache_data(loadtxt)(filepath, dtype=str, ndmin=2)
-x, y = array[:, 1], list(map(float, array[:, 0]))
+array = cache_data(lambda: loadtxt(stdin, dtype=str))()
+x = range(len(array)) if array.ndim == 1 else array[:, 0]
+y = (array if array.ndim == 1 else array[:, 1]).astype(float)
 highlight = text_input("Highlight")
 figure = bar(x=x, y=y)
 if highlight:
@@ -15,7 +15,7 @@ if highlight:
         x0=highlight,
         y0=0,
         x1=highlight,
-        y1=max(y),
-        line=dict(color="orange", dash="dot"),
+        y1=y.max(),
+        line={"color": "orange", "dash": "dot"},
     )
 plotly_chart(figure)
