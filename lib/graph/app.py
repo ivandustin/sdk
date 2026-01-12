05184e08a7e1df77170abe75658a8dc2b594a79f
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2026-01-12 21:59:41 +0800

    Clean up graph

--- lib/graph/app.py
+++ lib/graph/app.py
@@ -4,8 +4,6 @@ from plotly.express import line
 from numpy import loadtxt
 
 set_page_config(layout="wide")
-array = cache_data(lambda: loadtxt(stdin, dtype=float))()
-x = range(len(array)) if array.ndim == 1 else array[:, 0]
-y = (array if array.ndim == 1 else array[:, 1]).astype(float)
-figure = line(x=x, y=y)
+values = cache_data(lambda: loadtxt(stdin, dtype=float))()
+figure = line(y=values)
 plotly_chart(figure)
