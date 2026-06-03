5e3a5bdb7492d39d5e45a879862af9fe71638489
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2026-06-03 15:26:15 +0000

    Remove button in bar2

--- lib/st/bar2.py
+++ lib/st/bar2.py
@@ -1,27 +1,26 @@
 from sys import argv
-from streamlit import set_page_config, button, plotly_chart
+from streamlit import set_page_config, plotly_chart
 from plotly.express import bar
 from numpy import loadtxt
 
 
 def main():
-    if button("Load", shortcut="L"):
-        xpath = argv[1]
-        ypath = argv[2]
-        zpath = argv[3]
-        x = loadtxt(xpath)
-        y = loadtxt(ypath)[-1]
-        z = loadtxt(zpath)[-1][-1]
-        figure = bar(x=x, y=y)
-        figure.add_shape(
-            type="line",
-            x0=z,
-            y0=0,
-            x1=z,
-            y1=y.max(),
-            line={"color": "orange", "dash": "dot"},
-        )
-        plotly_chart(figure)
+    xpath = argv[1]
+    ypath = argv[2]
+    zpath = argv[3]
+    x = loadtxt(xpath)
+    y = loadtxt(ypath)[-1]
+    z = loadtxt(zpath)[-1][-1]
+    figure = bar(x=x, y=y)
+    figure.add_shape(
+        type="line",
+        x0=z,
+        y0=0,
+        x1=z,
+        y1=y.max(),
+        line={"color": "orange", "dash": "dot"},
+    )
+    plotly_chart(figure)
 
 
 if __name__ == "__main__":
